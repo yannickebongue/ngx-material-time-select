f39731baffcecd0e97242cbf39336b60c4a77636
Yannick Ebongue <yannickebongue@yahoo.fr>
2019-02-06 10:28:55 +0100

    Add time select demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import {MatTimeSelectModule} from '../../projects/time-select/src/lib/time-select.module';
+import {MatNativeTimeModule} from '../../projects/time-select/src/lib/native-time.module';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {TimeSelectDemoComponent} from './time-select-demo/time-select-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeSelectDemoComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    OverlayModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTimeSelectModule,
+    MatNativeTimeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
